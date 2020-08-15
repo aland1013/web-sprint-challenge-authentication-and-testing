abc16f67a2eb396754c7be9ede190620970b811d
aland1013 <adam.land@gmail.com>
2020-08-14 21:44:57 -0400

    added restricted middleware to GET /api/jokes route

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,23 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../config/secrets');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const [authType, token] = req.headers.authorization.split(' ');
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'invalid token' });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
