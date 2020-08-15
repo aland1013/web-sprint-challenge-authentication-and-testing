475d934ac89f96e2aef4a22affab20c89ffbcb6c
aland1013 <adam.land@gmail.com>
2020-08-14 22:41:38 -0400

    finished POST /register tests

---
+++ auth/auth-router.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+describe('auth-router.test.js', () => {
+  it('should set up testing environment', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  });
+});
+
+describe('POST /register', () => {
+  let res = {};
+
+  beforeAll(async () => {
+    await db('users').truncate();
+
+    res = await request(server).post('/api/auth/register').send({
+      username: 'user',
+      password: 'password'
+    });
+  });
+
+  it('should return 201', () => {
+    expect(res.status).toBe(201);
+  });
+
+  it('should return a JSON object', () => {
+    expect(res.type).toBe('application/json');
+  });
+});
--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,22 @@ module.exports = {
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
-      tableName: 'dbmigrations',
+      tableName: 'dbmigrations'
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: './database/seeds' }
   },
+
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/test.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  }
 };
